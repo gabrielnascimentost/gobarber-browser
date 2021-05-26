1be6dd0dd5bdf00903d047728a92f94161d8f270
Gabriel Nascimento <gabrielstnascimento@outlook.com>
2021-05-26 16:29:46 -0300

    ajusting app

--- src/utils/getValidationErrors.ts
+++ src/utils/getValidationErrors.ts
@@ -8,8 +8,7 @@ export default function getValidationErrors(err: ValidationError): Errors {
     const validationErrors: Errors = {};
 
     err.inner.forEach(error => {
-        if (error.path !== undefined)
-            validationErrors[error.path] = error.message;
+        validationErrors[error.path] = error.message;
     });
 
     return validationErrors;
